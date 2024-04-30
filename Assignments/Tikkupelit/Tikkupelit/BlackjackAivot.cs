ec2ddedba41150db57bdf31407efd451d9bf9871
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-30 12:24:01 +0300

    BlackJack

--- Assignments/Tikkupelit/Tikkupelit/BlackjackAivot.cs
+++ Assignments/Tikkupelit/Tikkupelit/BlackjackAivot.cs
@@ -103,8 +103,69 @@ namespace Tikkupelit
         }
 		public bool AnnounceBlackjackWinner(List<KorttiPelaaja> players)
 		{
+			int playerNum = 0;
+			KorttiPelaaja currentWinner = players[playerNum];
+			playerNum++;
+			string winner = "Player";
+			while(playerNum < players.Count)
+			{
+				int currentHighest = rules.BlackjackCounter(currentWinner.korttiKasi);
+				int challengerScore = rules.BlackjackCounter(players[playerNum].korttiKasi);
+				if (currentHighest > 21)
+				{
+					currentHighest = 0;
+				}
+				string challengerName = players[playerNum].PelaajaTyyppi.ToString();
+				if (players[playerNum].PelaajaTyyppi != KorttiPelaajatyyppi.dealeri)
+				{
+					challengerName += " " + (playerNum + 1);
+				}
 
-		}
+				if(currentHighest == challengerScore)
+				{
+					winner += ", " + challengerName;
+					if (players[playerNum].PelaajaTyyppi == KorttiPelaajatyyppi.dealeri)
+					{
+						currentWinner = players[playerNum];
+						winner = challengerName;
+					}
+				}
+                else if (challengerScore > currentHighest && challengerScore <= 21)
+                {
+                    currentWinner = players[playerNum];
+                    winner = challengerName;
+                }
+                playerNum++;
+            }
+            if (rules.BlackjackCounter(currentWinner.korttiKasi) > 21)
+            {
+                Console.WriteLine("Everyone lose");
+            }
+            else
+            {
+                Console.WriteLine(winner + " wins");
+                //for (int i = 0; i < players.Count; i++)
+                //{
+                //    string playerName = players[i].PelaajaTyyppi.ToString() + " " + (i + 1);
+                //    if (players[i].PelaajaTyyppi == PelaajaTyyppi.user)
+                //    {
+                //        playerName = "Player 1";
+                //    }
+                //    if (winner.Contains(playerName))
+                //    {
+                //        rules.BlackjackPayout(players[i], 2);
+                //    }
+                //}
+            }
+            Console.WriteLine();
+            Console.WriteLine("Do you want to play again: \"y\"");
+            char userContinue = Char.ToLower(Console.ReadKey().KeyChar);
+            if (userContinue == 'y')
+            {
+                return false;
+            }
+            return true;
+        }
 	}
 }
 
